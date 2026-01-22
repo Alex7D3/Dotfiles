96e969c0174ed0741b678a05b63a1ef444b83456
Alex <ao11@my.yorku.ca>
2026-01-22 18:45:53 -0500

    Update cmp settings

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,3 +1,40 @@
+local config = function()
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
+	cmp.setup({
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "path" },
+			{ name = "luasnip" },
+			{ name = "buffer",  keyword_length = 5 },
+		},
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			-- Navigate between completion items
+			["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+			["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+
+			["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+			-- Ctrl+Space to trigger completion menu
+			["<C-Space>"] = cmp.mapping.complete(),
+
+			-- Scroll up and down in the completion documentation
+			["<C-u>"] = cmp.mapping.scroll_docs(-4),
+			["<C-d>"] = cmp.mapping.scroll_docs(4),
+		}),
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+	})
+end
+
+
 return {
 	{
 		"L3MON4D3/LuaSnip",
@@ -13,42 +50,7 @@ return {
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			cmp.setup({
-				sources = {
-					{ name = "cmp_buffer" },
-					{ name = "cmp_path" },
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				},
-				mapping = cmp.mapping.preset.insert({
-					-- Navigate between completion items
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" }),
-
-					-- `Enter` key to confirm completion
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-					-- Ctrl+Space to trigger completion menu
-					["<C-Space>"] = cmp.mapping.complete(),
-
-					-- Scroll up and down in the completion documentation
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-				}),
-				-- snippet = {
-				-- 	expand = function(args)
-				-- 		vim.snippet.expand(args.body)
-				-- 	end,
-				-- },
-				window = {
-					documentation = cmp.config.window.bordered()
-				}
-			})
-		end,
-	}
+		config = config,
+	},
+	lazy = true,
 }
